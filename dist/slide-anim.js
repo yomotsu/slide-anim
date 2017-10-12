c7ab4e080212cc6731a5d98a519678a443162eb8
yomotsu <oyamada@pxgrid.com>
2017-10-12 20:37:58 +0900

    build for v0.1

--- dist/slide-anim.js
+++ dist/slide-anim.js
@@ -14,10 +14,10 @@
 
 		_: [],
 
-		add: function add(el, timeoutId, onCancelled) {
+		add: function add(el, defaultStyle, timeoutId, onCancelled) {
 
 			inAnimItems.remove(el);
-			inAnimItems._.push({ el: el, timeoutId: timeoutId, onCancelled: onCancelled });
+			inAnimItems._.push({ el: el, defaultStyle: defaultStyle, timeoutId: timeoutId, onCancelled: onCancelled });
 		},
 		remove: function remove(el) {
 
@@ -30,6 +30,10 @@
 			clearTimeout(inAnimItem.timeoutId);
 			inAnimItem.onCancelled();
 			inAnimItems._.splice(index, 1);
+		},
+		find: function find(el) {
+
+			return inAnimItems._[inAnimItems.findIndex(el)];
 		},
 		findIndex: function findIndex(el) {
 
@@ -56,12 +60,15 @@
 
 		var defaultStyle = el.getAttribute('style') || '';
 		var style = window.getComputedStyle(el);
+		var defaultStyles = getDefaultStyles(el);
 		var isBorderBox = /border-box/.test(style.getPropertyValue('box-sizing'));
-		var paddingTop = +style.getPropertyValue('padding-top').replace(/px/, '');
-		var paddingBottom = +style.getPropertyValue('padding-bottom').replace(/px/, '');
-		var borderTop = +style.getPropertyValue('border-top-width').replace(/px/, '');
-		var borderBottom = +style.getPropertyValue('border-bottom-width').replace(/px/, '');
-		var contentHeight = getHiddenHeight(el);
+
+		var contentHeight = defaultStyles.height;
+		var paddingTop = +defaultStyles.paddingTop;
+		var paddingBottom = +defaultStyles.paddingBottom;
+		var borderTop = +defaultStyles.borderTopWidth;
+		var borderBottom = +defaultStyles.borderBottomWidth;
+
 		var duration = options && options.duration || 400;
 		var cssDuration = duration + 'ms';
 		var cssEasing = CSS_EASEOUT_EXPO;
@@ -97,13 +104,14 @@
 
 		var timeoutId = setTimeout(function () {
 
-			el.setAttribute('style', defaultStyle);
+			// el.setAttribute( 'style', defaultStyle );
+			resetStyle(el);
 			el.style.display = 'block';
 			inAnimItems.remove(el);
 			onComplete();
 		}, duration);
 
-		inAnimItems.add(el, timeoutId, onCancelled);
+		inAnimItems.add(el, defaultStyle, timeoutId, onCancelled);
 	}
 
 	function slideUp(el, options) {
@@ -150,32 +158,88 @@
 
 		var timeoutId = setTimeout(function () {
 
-			el.setAttribute('style', defaultStyle);
+			// el.setAttribute( 'style', defaultStyle );
+			resetStyle(el);
 			el.style.display = 'none';
 			inAnimItems.remove(el);
 			onComplete();
 		}, duration);
 
-		inAnimItems.add(el, timeoutId, onCancelled);
+		inAnimItems.add(el, defaultStyle, timeoutId, onCancelled);
 	}
 
-	function getHiddenHeight(el) {
+	function slideStop(el) {
+
+		var elementObject = inAnimItems.find(el);
+
+		if (!elementObject) return;
+
+		var defaultStyle = elementObject.defaultStyle;
+		var style = window.getComputedStyle(el);
+		var height = style.height;
+		var paddingTop = style.paddingTop;
+		var paddingBottom = style.paddingBottom;
+		var borderTopWidth = style.borderTopWidth;
+		var borderBottomWidth = style.borderBottomWidth;
+
+		// el.setAttribute( 'style', defaultStyle );
+		resetStyle(el);
+		el.style.height = height;
+		el.style.paddingTop = paddingTop;
+		el.style.paddingBottom = paddingBottom;
+		el.style.borderTopWidth = borderTopWidth;
+		el.style.borderBottomWidth = borderBottomWidth;
+		el.style.overflow = 'hidden';
+		inAnimItems.remove(el);
+	}
+
+	function resetStyle(el) {
+
+		el.style.visibility = null;
+		el.style.height = null;
+		el.style.paddingTop = null;
+		el.style.paddingBottom = null;
+		el.style.borderTopWidth = null;
+		el.style.borderBottomWidth = null;
+		el.style.overflow = null;
+		el.style.transition = null;
+		el.style.webkitTransition = null;
+	}
+
+	function getDefaultStyles(el) {
 
 		var defaultStyle = el.getAttribute('style') || '';
 
 		el.style.position = 'absolute';
 		el.style.visibility = 'hidden';
 		el.style.display = 'block';
+		el.style.height = null;
+		el.style.paddingTop = null;
+		el.style.paddingBottom = null;
+		el.style.borderTopWidth = null;
+		el.style.borderBottomWidth = null;
 
+		var style = window.getComputedStyle(el);
+		var paddingTop = +style.getPropertyValue('padding-top').replace(/px/, '');
+		var paddingBottom = +style.getPropertyValue('padding-bottom').replace(/px/, '');
+		var borderTop = +style.getPropertyValue('border-top-width').replace(/px/, '');
+		var borderBottom = +style.getPropertyValue('border-bottom-width').replace(/px/, '');
 		var height = el.scrollHeight;
 
 		el.setAttribute('style', defaultStyle);
 
-		return height;
+		return {
+			height: height,
+			paddingTop: paddingTop,
+			paddingBottom: paddingBottom,
+			borderTop: borderTop,
+			borderBottom: borderBottom
+		};
 	}
 
 	exports.slideDown = slideDown;
 	exports.slideUp = slideUp;
+	exports.slideStop = slideStop;
 
 	Object.defineProperty(exports, '__esModule', { value: true });
 
